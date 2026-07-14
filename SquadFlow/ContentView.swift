835e68353dabc5429fc796854557626398498087
Juan Adolfo Velazquez Reyes <velazquezjuanadolfo472@gmail.com>
2026-07-13 18:30:49 -0600

    Initial Commit

---
+++ SquadFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SquadFlow
+//
+//  Created by Juan Adolfo Velazquez Reyes on 13/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SquadFlow/SquadFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquadFlowApp.swift
+//  SquadFlow
+//
+//  Created by Juan Adolfo Velazquez Reyes on 13/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SquadFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
