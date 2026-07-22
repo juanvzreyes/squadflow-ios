be137b7340a96515779f52096bd20bce54e215a9
Juan Adolfo Velazquez Reyes <velazquezjuanadolfo472@gmail.com>
2026-07-22 14:45:50 -0600

    feat: add components views

---
+++ SquadFlow/Shared/Components/LoadingStateView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingStateView.swift
+//  SquadFlow
+//
+//  Created by Juan Adolfo Velazquez Reyes on 22/07/26.
+//
+
+import SwiftUI
+
+struct LoadingStateView: View {
+    var body: some View {
+        ProgressView("Cargando información...")
+    }
+}
+
+#Preview {
+    LoadingStateView()
+}
